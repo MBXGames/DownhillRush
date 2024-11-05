4af16546aa32f9b95b86d2d7b41ad3c2fb0fbede
Alejandro Pérez Carretero <96301176+AlexPC03@users.noreply.github.com>
2024-11-05 11:27:24 +0100

    Modelos de obstáculos primer circuito

--- Assets/Scripts/BigObstacleController.cs
+++ Assets/Scripts/BigObstacleController.cs
@@ -4,8 +4,34 @@ using UnityEngine;
 
 public class BigObstacleController : MonoBehaviour
 {
+    public int track = 1;
+    public GameObject[] modelsTrack1;
+    public GameObject[] modelsTrack2;
+    public GameObject[] modelsTrack3;
+    public MeshRenderer exampleModel;
     public float dodgeBoost;
     public int dodgePoints;
+
+    private void Start()
+    {
+        if (exampleModel != null)
+        {
+            exampleModel.enabled = false;
+        }
+        switch (track)
+        {
+            case 2:
+                modelsTrack2[Random.Range(0, modelsTrack2.Length)].SetActive(true);
+                break;
+            case 3:
+                modelsTrack3[Random.Range(0, modelsTrack3.Length)].SetActive(true);
+                break;
+            case 1:
+            default:
+                modelsTrack1[Random.Range(0, modelsTrack1.Length)].SetActive(true);
+                break;
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
--- Assets/Scripts/SmallObstacleController.cs
+++ Assets/Scripts/SmallObstacleController.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class SmallObstacleController : MonoBehaviour
 {
+    private GameObject model;
+    public int track = 1;
+    public GameObject[] modelsTrack1;
+    public GameObject[] modelsTrack2;
+    public GameObject[] modelsTrack3;
+    public MeshRenderer exampleModel;
     public float speedReduction;
+
+    private void Start()
+    {
+        if (exampleModel != null)
+        {
+            exampleModel.enabled = false;
+        }
+        switch (track)
+        {
+            case 2:
+                model = modelsTrack2[Random.Range(0, modelsTrack2.Length)];
+                break;
+            case 3:
+                model = modelsTrack3[Random.Range(0, modelsTrack3.Length)];
+                break;
+            case 1:
+            default:
+                model = modelsTrack1[Random.Range(0, modelsTrack1.Length)];
+                break;
+        }
+        model.SetActive(true);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             other.gameObject.GetComponent<PlayerController>().SmallHit(speedReduction);
+            if (model.GetComponent<Animator>()!=null)
+            {
+                model.GetComponent<Animator>().SetTrigger("Collide");
+            }
         }
     }
 }
